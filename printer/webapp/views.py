8f5fd961217acb5489714c42e2a2993f23f0a375
ashwin347 <ash347@users.noreply.github.com>
2020-04-23 01:10:12 +0530

    django2

--- printer/webapp/views.py
+++ printer/webapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+import os
+import json
 def index(request):
-    return render(request,'index.html');
+    clients=['static/img/clients/'+name for name in os.listdir('static/img/clients')]
+    f = open ('static/main.json', "r")
+    info=json.loads(f.read())
+    print(info['phone'])
+    return render(request,'index.html',{'clients':clients,'info':info})
